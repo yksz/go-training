f27baf713eb2b7c5532cdd1f7b3790619d5fe99b
yksz <user4git@gmail.com>
2016-01-12 00:17:43 +0900

    Add ch01/ex04

---
+++ ch01/ex04/dup.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	filenames := make(map[string][]string)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts, filenames)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup: %v\n", err)
+				continue
+			}
+			countLines(f, counts, filenames)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\t%s\n", n, line, filenames[line])
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int, filenames map[string][]string) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		line := input.Text()
+		counts[line]++
+		if !contains(filenames[line], f.Name()) {
+			filenames[line] = append(filenames[line], f.Name())
+		}
+	}
+}
+
+func contains(slice []string, target string) bool {
+	for _, v := range slice {
+		if v == target {
+			return true
+		}
+	}
+	return false
+}
