59bcfee17a94b89caf34ff4723967ce64a58a25f
yksz <user4git@gmail.com>
2016-01-19 00:40:10 +0900

    Add ch01/ex11

---
+++ ch01/ex11/run.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+go run fetchall.go \
+http://google.com/ \
+http://facebook.com/ \
+http://youtube.com/ \
+http://baidu.com/ \
+http://yahoo.com/ \
+http://amazon.com/ \
+http://wikipedia.org/ \
+http://qq.com/ \
+http://google.co.in/ \
+http://twitter.com/ \
+http://taobao.com/ \
+http://live.com/ \
+http://sina.com.cn/ \
+http://yahoo.co.jp/ \
+http://weibo.com/ \
+http://msn.com/ \
+http://google.co.jp/ \
+http://vk.com/ \
+http://bing.com/ \
+http://yandex.ru/ \
+http://linkedin.com/ \
+http://hao123.com/ \
+http://ebay.com/ \
+http://amazon.co.jp/ \
+http://instagram.com/ \
