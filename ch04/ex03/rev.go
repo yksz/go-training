c7fdc877736a245f5422e487b228b7d54ffb9151
yksz <user4git@gmail.com>
2016-03-13 21:16:21 +0900

    Add ch04/ex03

---
+++ ch04/ex03/rev.go
@@ -0,0 +1,17 @@
+// Rev reverses a array.
+package main
+
+import "fmt"
+
+func main() {
+	a := [...]int{0, 1, 2, 3, 4, 5}
+	reverse(&a)
+	fmt.Println(a) // "[5 4 3 2 1 0]"
+}
+
+// reverse reverses a array pointer of ints in place.
+func reverse(a *[6]int) {
+	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
+	}
+}
