11bd811e42ba3a83988a0b390ce5972e8ca7d1c9
yksz <user4git@gmail.com>
2016-03-26 23:43:48 +0900

    Correct ch04/ex07

--- ch04/ex07/rev.go
+++ ch04/ex07/rev.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	s := "0 1 2 3 4 5"
+	s := "Hello, 世界"
 	b := []byte(s)
-	reverse(b)
-	fmt.Printf("%q\n", b) // "5 4 3 2 1 0"
+	b = reverse(b)
+	fmt.Printf("%q\n", b) // "界世 ,olleH"
 }
 
-func reverse(b []byte) {
-	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
-		b[i], b[j] = b[j], b[i]
+func reverse(b []byte) []byte {
+	r := []rune(string(b))
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
 	}
+	return []byte(string(r))
 }
