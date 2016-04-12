a3ec5c7014b5ae6db0476994994e8fd8729a0bbc
yksz <yksz.dev@gmail.com>
2016-04-13 02:13:51 +0900

    Add ch05/ex05

---
+++ ch05/ex05/count.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		words, images, err := CountWordsAndImages(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "count: %v\n", err)
+			continue
+		}
+		fmt.Printf("words\timages\n")
+		fmt.Printf("%d\t%d\n", words, images)
+	}
+}
+
+// CountWordsAndImages does an HTTP GET request for the HTML
+// document url and returns the number of words and images in it.
+func CountWordsAndImages(url string) (words, images int, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		err = fmt.Errorf("parsing HTML: %s", err)
+		return
+	}
+	words, images = countWordsAndImages(doc)
+	return
+}
+
+func countWordsAndImages(n *html.Node) (words, images int) {
+	scanner := bufio.NewScanner(strings.NewReader(n.Data))
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		words++
+	}
+	if n.Type == html.ElementNode && n.Data == "img" {
+		images++
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ws, is := countWordsAndImages(c)
+		words += ws
+		images += is
+	}
+	return words, images
+}
