9ca685303335dd577b1c24a2226818d516be0e78
yksz <yksz.dev@gmail.com>
2016-04-19 00:59:20 +0900

    Add ch05/ex07

---
+++ ch05/ex07/outline.go
@@ -0,0 +1,90 @@
+// Outline prints the outline of an HTML document tree.
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		outline(url)
+	}
+}
+
+func outline(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	forEachNode(doc, startElement, endElement)
+
+	return nil
+}
+
+// forEachNode calls the functions pre(x) and post(x) for each node
+// x in the tree rooted at n. Both functions are optional.
+// pre is called before the children are visited (preorder) and
+// post is called after (postorder).
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
+
+var depth int
+
+func startElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		if n.FirstChild != nil {
+			fmt.Printf("%*s<%s%s>\n", depth*2, "", n.Data, getAttrs(n))
+			s := strings.TrimSpace(n.FirstChild.Data)
+			if s != "" {
+				fmt.Printf("%*s%s\n", (depth+1)*2, "", formatText(s, depth+1))
+			}
+		}
+		depth++
+	}
+}
+
+func formatText(s string, depth int) string {
+	return strings.Replace(s, "\n", "\n"+fmt.Sprintf("%*s", depth*2, ""), -1)
+}
+
+func endElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		depth--
+		if n.FirstChild != nil {
+			fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+		} else {
+			fmt.Printf("%*s<%s%s/>\n", depth*2, "", n.Data, getAttrs(n))
+		}
+	}
+}
+
+func getAttrs(n *html.Node) string {
+	s := ""
+	for _, a := range n.Attr {
+		s += fmt.Sprintf(" %s=\"%s\"", a.Key, a.Val)
+	}
+	return s
+}
