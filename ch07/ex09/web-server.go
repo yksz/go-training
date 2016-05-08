7c758c1e15563a71061f9690a1c3e5f6fd702b73
yksz <yksz.dev@gmail.com>
2016-05-08 19:40:18 +0900

    Update ch07/ex09

--- ch07/ex09/web-server.go
+++ ch07/ex09/web-server.go
@@ -6,15 +6,9 @@ import (
 	"net/http"
 	"sort"
 	"time"
-)
 
-type Track struct {
-	Title  string
-	Artist string
-	Album  string
-	Year   int
-	Length time.Duration
-}
+	. "../ex08/sorting"
+)
 
 var tracks = []*Track{
 	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
@@ -31,63 +25,34 @@ func length(s string) time.Duration {
 	return d
 }
 
-type byTitle []*Track
-
-func (x byTitle) Len() int           { return len(x) }
-func (x byTitle) Less(i, j int) bool { return x[i].Title < x[j].Title }
-func (x byTitle) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
-
-type byArtist []*Track
-
-func (x byArtist) Len() int           { return len(x) }
-func (x byArtist) Less(i, j int) bool { return x[i].Artist < x[j].Artist }
-func (x byArtist) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
-
-type byAlbum []*Track
-
-func (x byAlbum) Len() int           { return len(x) }
-func (x byAlbum) Less(i, j int) bool { return x[i].Album < x[j].Album }
-func (x byAlbum) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
-
-type byYear []*Track
-
-func (x byYear) Len() int           { return len(x) }
-func (x byYear) Less(i, j int) bool { return x[i].Year < x[j].Year }
-func (x byYear) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
-
-type byLength []*Track
-
-func (x byLength) Len() int           { return len(x) }
-func (x byLength) Less(i, j int) bool { return x[i].Length < x[j].Length }
-func (x byLength) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
-
-var trackTemplate = template.Must(template.ParseFiles("templates/track.html"))
-
 func main() {
+	customSort := CustomSort{}
+	trackTemplate := template.Must(template.ParseFiles("templates/track.html"))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 		if err := r.ParseForm(); err != nil {
 			log.Print(err)
 		}
+
+		t := copyTracks(tracks)
 		sortKey := r.FormValue("sort")
 		if sortKey != "" {
-			switch sortKey {
-			case "title":
-				sort.Sort(byTitle(tracks))
-			case "artist":
-				sort.Sort(byArtist(tracks))
-			case "album":
-				sort.Sort(byAlbum(tracks))
-			case "year":
-				sort.Sort(byYear(tracks))
-			case "length":
-				sort.Sort(byLength(tracks))
-			}
+			sort.Sort(customSort.Sort(t, sortKey))
 		}
-		if err := trackTemplate.Execute(w, tracks); err != nil {
+		if err := trackTemplate.Execute(w, t); err != nil {
 			log.Print(err)
 		}
 	})
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./static/js"))))
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func copyTracks(src []*Track) []*Track {
+	var dst []*Track
+	for _, v := range src {
+		t := *v
+		dst = append(dst, &t)
+	}
+	return dst
 }
