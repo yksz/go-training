b6a48083d555c28fd182f3c3abc8011f9437d579
yksz <yksz.dev@gmail.com>
2016-05-08 19:40:01 +0900

    Add ch07/ex08

---
+++ ch07/ex08/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"text/tabwriter"
+	"time"
+
+	. "./sorting"
+)
+
+var tracks = []*Track{
+	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+	{"Go", "Moby", "Moby", 1992, length("3m37s")},
+	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+}
+
+func length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
+}
+
+func printTracks(tracks []*Track) {
+	const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
+	for _, t := range tracks {
+		fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
+	}
+	tw.Flush() // calculate column widths and print table
+}
+
+type byTitle []*Track
+
+func (x byTitle) Len() int           { return len(x) }
+func (x byTitle) Less(i, j int) bool { return x[i].Title < x[j].Title }
+func (x byTitle) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+type byArtist []*Track
+
+func (x byArtist) Len() int           { return len(x) }
+func (x byArtist) Less(i, j int) bool { return x[i].Artist < x[j].Artist }
+func (x byArtist) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+type byAlbum []*Track
+
+func (x byAlbum) Len() int           { return len(x) }
+func (x byAlbum) Less(i, j int) bool { return x[i].Album < x[j].Album }
+func (x byAlbum) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+type byYear []*Track
+
+func (x byYear) Len() int           { return len(x) }
+func (x byYear) Less(i, j int) bool { return x[i].Year < x[j].Year }
+func (x byYear) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+type byLength []*Track
+
+func (x byLength) Len() int           { return len(x) }
+func (x byLength) Less(i, j int) bool { return x[i].Length < x[j].Length }
+func (x byLength) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+func copyTracks(src []*Track) []*Track {
+	var dst []*Track
+	for _, v := range src {
+		t := *v
+		dst = append(dst, &t)
+	}
+	return dst
+}
+
+func main() {
+	{
+		cs := CustomSort{}
+
+		fmt.Println("\nCustom: byYear:")
+		t1 := copyTracks(tracks)
+		sort.Sort(cs.Sort(t1, "year"))
+		printTracks(t1)
+
+		fmt.Println("\nCustom: byTitle:")
+		t2 := copyTracks(tracks)
+		sort.Sort(cs.Sort(t2, "title"))
+		printTracks(t2)
+	}
+	{
+		fmt.Println("\nStable:")
+		t := copyTracks(tracks)
+		sort.Stable(byYear(t))
+		sort.Stable(byTitle(t))
+		printTracks(t)
+	}
+}
---
+++ ch07/ex08/sorting/sorting.go
@@ -0,0 +1,69 @@
+package sorting
+
+import (
+	"time"
+)
+
+type Track struct {
+	Title  string
+	Artist string
+	Album  string
+	Year   int
+	Length time.Duration
+}
+
+type CustomSort struct {
+	t    []*Track
+	keys []string
+}
+
+func (x *CustomSort) Sort(t []*Track, key string) *CustomSort {
+	x.t = t
+
+	// remove key from x.keys
+	newKeys := []string{}
+	for _, v := range x.keys {
+		if v != key {
+			newKeys = append(newKeys, v)
+		}
+	}
+	// append key to the head
+	x.keys = append([]string{key}, newKeys...)
+	return x
+}
+
+func (x CustomSort) Len() int {
+	return len(x.t)
+}
+
+func (x CustomSort) Less(i, j int) bool {
+	for _, key := range x.keys {
+		switch key {
+		case "title":
+			if x.t[i].Title != x.t[j].Title {
+				return x.t[i].Title < x.t[j].Title
+			}
+		case "artist":
+			if x.t[i].Artist != x.t[j].Artist {
+				return x.t[i].Artist < x.t[j].Artist
+			}
+		case "album":
+			if x.t[i].Album != x.t[j].Album {
+				return x.t[i].Album < x.t[j].Album
+			}
+		case "year":
+			if x.t[i].Year != x.t[j].Year {
+				return x.t[i].Year < x.t[j].Year
+			}
+		case "length":
+			if x.t[i].Length != x.t[j].Length {
+				return x.t[i].Length < x.t[j].Length
+			}
+		}
+	}
+	return false
+}
+
+func (x CustomSort) Swap(i, j int) {
+	x.t[i], x.t[j] = x.t[j], x.t[i]
+}
