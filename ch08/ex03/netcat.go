14fced40343d30e7b6601f95311f6fc2eeb7c574
yksz <yksz.dev@gmail.com>
2016-06-23 13:44:33 +0900

    Add ch08/ex03

---
+++ ch08/ex03/netcat.go
@@ -0,0 +1,40 @@
+// Netcat is a simple read/write client for TCP servers.
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
+		log.Println("done")
+		done <- struct{}{} // signal the main goroutine
+	}()
+	mustCopy(conn, os.Stdin)
+	<-done // wait for background goroutine to finish
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	defer func() {
+		if conn, ok := dst.(*net.TCPConn); ok {
+			conn.CloseWrite()
+		}
+	}()
+	_, err := io.Copy(dst, src)
+	if err == io.EOF {
+		return
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+}
