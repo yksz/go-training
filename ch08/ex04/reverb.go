f933fe02d191951c1fd1d5cd5d2a1f6e59970b8c
yksz <yksz.dev@gmail.com>
2016-06-25 17:11:20 +0900

    Add ch08/ex04

---
+++ ch08/ex04/reverb.go
@@ -0,0 +1,53 @@
+// Reverb is a TCP server that simulates an echo.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	var wg sync.WaitGroup
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			echo(c, input.Text(), 1*time.Second)
+		}()
+		go func() {
+			wg.Wait()
+			if c, ok := c.(*net.TCPConn); ok {
+				c.CloseWrite()
+			}
+		}()
+	}
+}
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+}
