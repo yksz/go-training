03b64b5dfcb1bfadef2bd30600a24d21f09e336f
yksz <yksz.dev@gmail.com>
2016-07-24 19:35:45 +0900

    Add ch09/ex03

---
+++ ch09/ex03/memo_test.go
@@ -0,0 +1,63 @@
+package memo_test
+
+import (
+	"strconv"
+	"testing"
+	"time"
+
+	"./memo"
+)
+
+func TestGet(t *testing.T) {
+	processingTime := 100 * time.Millisecond
+	processingTimeStr := strconv.FormatInt(processingTime.Nanoseconds(), 10)
+
+	m := memo.New(do)
+	defer m.Close()
+
+	// not cached
+	if elapsed := elapsedTime(func() { m.Get(processingTimeStr, nil) }); elapsed < processingTime {
+		t.Errorf("not cached: elapsed=%s", elapsed)
+	}
+	// cached
+	if elapsed := elapsedTime(func() { m.Get(processingTimeStr, nil) }); elapsed > processingTime {
+		t.Errorf("cached: elapsed=%s", elapsed)
+	}
+}
+
+func TestGetWithDone(t *testing.T) {
+	processingTime := 100 * time.Millisecond
+	processingTimeStr := strconv.FormatInt(processingTime.Nanoseconds(), 10)
+
+	m := memo.New(do)
+	defer m.Close()
+
+	done := make(chan struct{})
+	go func() {
+		done <- struct{}{}
+	}()
+
+	// done
+	if elapsed := elapsedTime(func() { m.Get(processingTimeStr, done) }); elapsed > processingTime {
+		t.Errorf("done: elapsed=%s", elapsed)
+	}
+	// not cached
+	if elapsed := elapsedTime(func() { m.Get(processingTimeStr, nil) }); elapsed < processingTime {
+		t.Errorf("not cached: elapsed=%s", elapsed)
+	}
+}
+
+func do(nanoseconds string) (interface{}, error) {
+	ns, err := strconv.Atoi(nanoseconds)
+	if err != nil {
+		return nil, err
+	}
+	time.Sleep(time.Duration(ns) * time.Nanosecond)
+	return ns, nil
+}
+
+func elapsedTime(f func()) time.Duration {
+	start := time.Now()
+	f()
+	return time.Since(start)
+}
