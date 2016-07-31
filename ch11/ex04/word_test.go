3d1ab825a95832674a182a7382c1234479b9e27c
yksz <yksz.dev@gmail.com>
2016-07-31 19:43:51 +0900

    Add ch11/ex04

---
+++ ch11/ex04/word_test.go
@@ -0,0 +1,43 @@
+package word
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"gopl.io/ch11/word2"
+)
+
+var symbols = []rune{',', '.', ' '}
+
+// randomPalindrome returns a palindrome whose length and contents
+// are derived from the pseudo-random number generator rng.
+func randomPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25) // random length up to 24
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		var r rune
+		if i%2 == 0 {
+			r = rune(rng.Intn(0x1000)) // random rune up to '\u0999'
+		} else {
+			r = symbols[rng.Intn(len(symbols))]
+		}
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	return string(runes)
+}
+
+func TestRandomPalindromes(t *testing.T) {
+	// Initialize a pseudo-random number generator.
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !word.IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = false", p)
+		}
+	}
+}
