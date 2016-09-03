6f95da7505481e14331aa8426ab8c73f038544b8
yksz <yksz.dev@gmail.com>
2016-09-03 17:44:09 +0900

    Add ch12/ex02

---
+++ ch12/ex02/display_test.go
@@ -0,0 +1,17 @@
+package display
+
+func Example_cycle() {
+	// a linked list that eats its own tail
+	type Cycle struct {
+		Value int
+		Tail  *Cycle
+	}
+	var c Cycle
+	c = Cycle{42, &c}
+	Display("c", c)
+	// Output:
+	// Display c (display.Cycle):
+	// c.Value = 42
+	// (*c.Tail).Value = 42
+	// (*(*c.Tail).Tail).Value = 42
+}
