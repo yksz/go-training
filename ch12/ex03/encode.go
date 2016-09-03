e486fa9a20a3704bc8630b4db39730d74f9ec849
yksz <yksz.dev@gmail.com>
2016-09-03 19:23:29 +0900

    Add ch12/ex03

---
+++ ch12/ex03/encode.go
@@ -0,0 +1,109 @@
+package sexpr
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+)
+
+// Marshal encodes a Go value in S-expression form.
+func Marshal(v interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	if err := encode(&buf, reflect.ValueOf(v)); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+// encode writes to buf an S-expression representation of v.
+func encode(buf *bytes.Buffer, v reflect.Value) error {
+	switch v.Kind() {
+	case reflect.Invalid:
+		buf.WriteString("nil")
+
+	case reflect.Bool:
+		if v.Bool() {
+			fmt.Fprint(buf, "t")
+		} else {
+			fmt.Fprint(buf, "nil")
+		}
+
+	case reflect.Float32, reflect.Float64:
+		fmt.Fprintf(buf, "%f", v.Float())
+
+	case reflect.Complex64, reflect.Complex128:
+		n := v.Complex()
+		fmt.Fprintf(buf, "#C(%f %f)", real(n), imag(n))
+
+	case reflect.Interface:
+		if v.IsNil() {
+			fmt.Fprint(buf, "nil")
+		} else {
+			fmt.Fprintf(buf, "(\"%s\" ", v.Elem().Type())
+			encode(buf, v.Elem())
+			fmt.Fprint(buf, ")")
+		}
+
+	case reflect.Int, reflect.Int8, reflect.Int16,
+		reflect.Int32, reflect.Int64:
+		fmt.Fprintf(buf, "%d", v.Int())
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		fmt.Fprintf(buf, "%d", v.Uint())
+
+	case reflect.String:
+		fmt.Fprintf(buf, "%q", v.String())
+
+	case reflect.Ptr:
+		return encode(buf, v.Elem())
+
+	case reflect.Array, reflect.Slice: // (value ...)
+		buf.WriteByte('(')
+		for i := 0; i < v.Len(); i++ {
+			if i > 0 {
+				buf.WriteByte(' ')
+			}
+			if err := encode(buf, v.Index(i)); err != nil {
+				return err
+			}
+		}
+		buf.WriteByte(')')
+
+	case reflect.Struct: // ((name value) ...)
+		buf.WriteByte('(')
+		for i := 0; i < v.NumField(); i++ {
+			if i > 0 {
+				buf.WriteByte(' ')
+			}
+			fmt.Fprintf(buf, "(%s ", v.Type().Field(i).Name)
+			if err := encode(buf, v.Field(i)); err != nil {
+				return err
+			}
+			buf.WriteByte(')')
+		}
+		buf.WriteByte(')')
+
+	case reflect.Map: // ((key value) ...)
+		buf.WriteByte('(')
+		for i, key := range v.MapKeys() {
+			if i > 0 {
+				buf.WriteByte(' ')
+			}
+			buf.WriteByte('(')
+			if err := encode(buf, key); err != nil {
+				return err
+			}
+			buf.WriteByte(' ')
+			if err := encode(buf, v.MapIndex(key)); err != nil {
+				return err
+			}
+			buf.WriteByte(')')
+		}
+		buf.WriteByte(')')
+
+	default: // float, complex, bool, chan, func, interface
+		return fmt.Errorf("unsupported type: %s", v.Type())
+	}
+	return nil
+}
---
+++ ch12/ex03/sexpr_test.go
@@ -0,0 +1,43 @@
+package sexpr
+
+import "fmt"
+
+func Example1() {
+	type object struct {
+		Bool      bool
+		Float32   float32
+		Complex64 complex64
+		Interface interface{}
+	}
+	obj := object{
+		Bool:      true,
+		Float32:   1.2,
+		Complex64: 1 + 2i,
+		Interface: []int{1, 2, 3},
+	}
+
+	data, _ := Marshal(obj)
+	fmt.Printf("%s\n", data)
+	// Output:
+	// ((Bool t) (Float32 1.200000) (Complex64 #C(1.000000 2.000000)) (Interface ("[]int" (1 2 3))))
+}
+
+func Example2() {
+	type object struct {
+		Bool       bool
+		Float64    float64
+		Complex128 complex128
+		Interface  interface{}
+	}
+	obj := object{
+		Bool:       true,
+		Float64:    3.4,
+		Complex128: 3 + 4i,
+		Interface:  []string{"1", "2", "3"},
+	}
+
+	data, _ := Marshal(obj)
+	fmt.Printf("%s\n", data)
+	// Output:
+	// ((Bool t) (Float64 3.400000) (Complex128 #C(3.000000 4.000000)) (Interface ("[]string" ("1" "2" "3"))))
+}
